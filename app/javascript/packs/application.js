a58dc71e809765d77cbcd6a549c567ea527bfdf0
rsaito1216 <mr88888888sr@gmail.com>
2020-11-13 16:21:53 +0900

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,9 +13,6 @@ import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
-import '@fullcalendar/core/main.css';
-import '@fullcalendar/daygrid/main.css';
-import '@fullcalendar/timegrid/main.css';
     
 document.addEventListener("turbolinks:load", function() {
   var calendarEl = document.getElementById('calendar');
