7d5ffc8b146d6d5ff493e2311058de5124006742
rsaito1216 <mr88888888sr@gmail.com>
2020-11-16 16:15:35 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = {  host: 'art-calendar.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'art-calendar.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
