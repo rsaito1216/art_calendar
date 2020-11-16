49054e2ca0b3621d863c22f9fb0be7ab1eef281c
rsaito1216 <mr88888888sr@gmail.com>
2020-11-16 16:54:21 +0900

    訂正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
       if @user.save
-        UserMailer.send_mail(@user).deliver
         redirect_to root_path
       else
         render 'new'
