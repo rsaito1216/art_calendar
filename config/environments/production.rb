5f6f0eff0a58b21489cab0a628111b84a49507cf
rsaito1216 <mr88888888sr@gmail.com>
2020-11-17 12:07:53 +0900

    sendgridの前準備

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,11 +113,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { protocol: 'https',host:'art-calendar.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
+    address: 'smtp.sendgrid.net',
     port: 587,
-    domain: 'gmail.com',
-    user_name: ENV['EMAIL_ADDRESS'],
-    password: ENV['EMAIL_PASSWORDS'],
+    domain: 'heroku.com',
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
